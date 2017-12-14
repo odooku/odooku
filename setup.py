16f14d635330524fd5e0c7cc8a65714efc682c94
Raymond Reggers <raymond@adaptiv.nl>
2017-12-14 11:26:55 +0100

    fix namespace packages

--- setup.py
+++ setup.py
@@ -8,14 +8,18 @@ except(IOError, ImportError):
 
 setup(
     name='odooku',
-    version='11.0.3',
+    version='11.0.4',
     url='https://github.com/odooku/odooku',
     author='Raymond Reggers - Adaptiv Design',
     author_email='raymond@adaptiv.nl',
     description=('Odooku'),
     long_description=long_description,
     license='Apache Software License',
-    packages=find_packages(),
+    packages=find_packages() + [
+        'odooku_addons',
+        'odooku_commands',
+        'odooku_patches'
+    ],
     include_package_data=True,
     zip_safe=False,
     python_requires='>=3.5',
